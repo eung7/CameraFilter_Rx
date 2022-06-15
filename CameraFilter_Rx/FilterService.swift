e6cd959ca7af830fd5b7948a5e2dc228f3141907
eung7 <wannabeeung@gmail.com>
2022-06-15 21:48:44 +0900

    [Add] Transforming Apply Filter into an Observable

--- CameraFilter_Rx/FilterService.swift
+++ CameraFilter_Rx/FilterService.swift
@@ -8,6 +8,7 @@
 import Foundation
 import UIKit
 import CoreImage
+import RxSwift
 
 class FilterService {
     private var context: CIContext
@@ -16,7 +17,16 @@ class FilterService {
         self.context = CIContext()
     }
     
-    func applyFilter(to inputImage: UIImage, completion: @escaping (UIImage) -> Void) {
+    func applyFilter(to inputImage: UIImage) -> Observable<UIImage> {
+        return Observable<UIImage>.create { observer in
+            self.applyFilter(to: inputImage) { filteredImage in
+                observer.onNext(filteredImage)
+            }
+            return Disposables.create()
+        }
+    }
+    
+    private func applyFilter(to inputImage: UIImage, completion: @escaping (UIImage) -> Void) {
         let filter = CIFilter(name: "CICMYKHalftone")!
         filter.setValue(5.0, forKey: kCIInputWidthKey)
         
@@ -29,3 +39,4 @@ class FilterService {
         }
     }
 }
+
--- CameraFilter_Rx/ViewController.swift
+++ CameraFilter_Rx/ViewController.swift
@@ -31,11 +31,12 @@ class ViewController: UIViewController {
     
     @IBAction func applyFilterButtonPressed() {
         guard let sourceImage = self.photoImageView.image else { return }
-        FilterService().applyFilter(to: sourceImage) { filteredImage in
-            DispatchQueue.main.async {
-                self.photoImageView.image = filteredImage
-            }
-        }
+        FilterService().applyFilter(to: sourceImage)
+            .subscribe(onNext: { filteredImage in
+                DispatchQueue.main.async {
+                    self.photoImageView.image = filteredImage
+                }
+            }).disposed(by: disposeBag)
     }
     
     private func updateUI(with image: UIImage) {
