65ef52266bb195da7e26cff51418c18f841f4d1b
eung7 <wannabeeung@gmail.com>
2022-06-15 19:10:14 +0900

    [Add] Setting Up the User Interface

---
+++ CameraFilter_Rx/PhotosCollectionViewController.swift
@@ -0,0 +1,14 @@
+//
+//  PhotosCollectionViewController.swift
+//  CameraFilter_Rx
+//
+//  Created by 김응철 on 2022/06/15.
+//
+
+import UIKit
+import RxCocoa
+import RxSwift
+
+class PhotosCollectionViewController: UICollectionViewController {
+    
+}
